4abafb02d24ea4c375d018829b140c9c428333cb
Rhystalgie <aufamuh12@gmail.com>
2025-01-03 13:52:38 +0700

    design_system

---
+++ lib/design_system/color_collection.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ColorCollection {
+  // Primary Colors
+  static const Color primary1 = Color(0xFF00AA13);
+  static const Color primary2 = Color(0xFFEE2737);
+  static const Color primary3 = Color(0xFFFF6700);
+  static const Color primary4 = Color(0xFF00AED6);
+  static const Color primary5 = Color(0xFF93328E);
+  static const Color primary6 = Color(0xFFDF1998);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Green Colors
+  static const Color lightGreen = Color(0xFF30B058);
+  static const Color greenGojek = Color(0xFF00AA13);
+  static const Color green = Color(0xFF006C02);
+  static const Color darkGreen = Color(0xFF004901);
+
+  // Greyscale
+  static const Color dark = Color(0xFF191919);
+  static const Color darkGrey = Color(0xFF9F9F9F);
+  static const Color grey = Color(0xFF727272);
+  static const Color lightGrey = Color(0xFFD7D7D7);
+}
---
+++ lib/design_system/typography_collection.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class TypographyCollection {
+  // Header Styles
+  static const TextStyle h1 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 30,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+
+  static const TextStyle h4 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  static const TextStyle h5 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  static const TextStyle h6 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle h7 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+
+  // Subhead & Body Styles
+  static const TextStyle subhead1 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const TextStyle subhead2 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle subhead3 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+
+  static const TextStyle paragraph1 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const TextStyle paragraph2 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle paragraph3 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static const TextStyle paragraph4 = TextStyle(
+    fontFamily: 'SFProText',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+}
